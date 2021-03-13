325a6786340a1a3e543dc03eb2c4e74f472a44da
Anas Hassari <anas.hassari@jemmic.com>
2021-03-13 12:54:23 +0100

    removing unused imports

--- src/main/java/com/ra/quotes/SpringQuotes/services/UserService.java
+++ src/main/java/com/ra/quotes/SpringQuotes/services/UserService.java
@@ -1,11 +1,8 @@
 package com.ra.quotes.SpringQuotes.services;
 
 import com.ra.quotes.SpringQuotes.datamodel.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 // ALL methods needed by us to return the user-specific services.
