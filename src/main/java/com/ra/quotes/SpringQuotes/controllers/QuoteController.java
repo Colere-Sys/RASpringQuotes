f8be74e53688202462aec3ac513bcbe7b4d0bb7b
Riham <rihamouassou@gmail.com>
2021-03-17 22:38:42 +0100

    SQ-5 Remove import

--- src/main/java/com/ra/quotes/SpringQuotes/controllers/QuoteController.java
+++ src/main/java/com/ra/quotes/SpringQuotes/controllers/QuoteController.java
@@ -1,7 +1,6 @@
 package com.ra.quotes.SpringQuotes.controllers;
 
 import com.ra.quotes.SpringQuotes.datamodel.Quote;
-import com.ra.quotes.SpringQuotes.datamodel.UserTO;
 import com.ra.quotes.SpringQuotes.services.QuoteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
